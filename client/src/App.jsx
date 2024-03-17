7689507224641e111df6df82174ddf3e3cc3c337
vignesh-yadav <vigneshyadav.dev@gmail.com>
2024-03-17 06:55:45 +0530

    created pages and added routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
 
 export default function App() {
   return (
-    <h1 className='text-3xl text-blue-500'>
-      vignesh yadav
-    </h1>
+ <BrowserRouter>
+ <Routes>
+  <Route paath="/" element={<Home/>} />
+  <Route paath="/about" element={<About/>} />
+  <Route paath="/sign-in" element={<SignIn/>} />
+  <Route paath="/sign-up" element={<SignUp/>} />
+  <Route paath="/dashboard" element={<Dashboard/>} />
+  <Route paath="/projects" element={<Projects/>} />
+ </Routes>
+ </BrowserRouter>
   )
 }
