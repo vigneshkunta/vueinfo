03925603e1c270932981c202682fc3811b4f92fd
vignesh-yadav <vigneshyadav.dev@gmail.com>
2024-03-25 20:43:59 +0530

    added functionality for signup and login pages

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,8 +1,62 @@
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
-export default function SignIn() {
+export default function SignIn() { 
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.email || !formData.password) {
+      return setErrorMessage('Please fill all the fields');
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if (res.ok) {
+        navigate('/');
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  };
   return (
-    <div>
-      Signin
+    <div id="allignment-container">
+    <div id="signup-container">
+        <div className="signup-logo">
+            <img src="logo.png" alt="Logo" />
+        </div>
+        <div className="signup-text">
+            <h2>Login to continue</h2>
+        </div>
+        <div className="signup-form-container">
+        <form className="signup-form" action="#" method="post" onSubmit={handleSubmit}>
+            <input type="email" id="email"  className="signup-input" name="email" placeholder="Email" required onChange={handleChange}/>
+            <input type="password" className="signup-input" placeholder="********" id="password" onChange={handleChange} />
+            <button type="submit" className="signup-button" id="signup-button">Sign In</button>
+        </form>
+        </div>
+        <div className="signup-signin-link">
+            <p>Don't have an account? <Link to="/sign-up">SignUp</Link></p>
+        </div>
     </div>
+        </div>
   )
 }
+
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,24 +1,43 @@
 import "./SignupPage.css";
-import { Link } from 'react-router-dom';
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
+import { Link, useNavigate } from 'react-router-dom';
 import { useState } from "react";
 
 
 const SignupPage = () => {
     const [formData, setFormData] = useState({});
+    const [errorMessage, setErrorMessage] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
     const handleChange = (e) => {
-       setFormData({ ...formData, [e.target.id]: e.target.value });
+        setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
     };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        if (!formData.username || !formData.email || !formData.password) {
+            return setErrorMessage('Please fill out all fields.');
+          } 
         try {
+            setLoading(true);
+            setErrorMessage(null);
             const res = await fetch('/api/auth/signup', {
                 method: 'POST',
                 headers: {'Content-Type':'application/json'},
                 body: JSON.stringify(formData),
             });
             const data = await res.json();
-        }catch (error){}
+            if (data.success === false) {
+                return setErrorMessage(data.message);
+              }
+              setLoading(false);
+              if(res.ok) {
+                navigate('/sign-in');
+              } 
+        }catch (error){
+            setErrorMessage(error.message);
+            setLoading(false);
+        }
     };
 
   return (
@@ -35,11 +54,27 @@ const SignupPage = () => {
         <input type="string" className="signup-input" id="username" name="username" placeholder="Username" required onChange={handleChange}/>
         <input type="email" id="email"  className="signup-input" name="email" placeholder="Email" required onChange={handleChange}/>
         <input type="password" className="signup-input" placeholder="Password" id="password" onChange={handleChange}/>
-        <button type="submit" className="signup-button" id="signup-button">Sign Up</button>
+        <button type="submit" className="signup-button" id="signup-button" disabled={loading}>
+        {loading ? (
+                <>
+                  <Spinner size='sm' />
+                  <span className='pl-3'>Loading...</span>
+                </>
+              ) : (
+                'Sign Up'
+              )}
+            </button>
     </form>
     </div>
     <div className="signup-signin-link">
         <p>Already have an account? <Link to="/sign-in">SignIn</Link></p>
+    </div>
+    <div>
+    {errorMessage && (
+            <Alert className='mt-5' color='failure'>
+              {errorMessage}
+            </Alert>
+          )}  
     </div>
 </div>
     </div>
