27549790ff3f58126ad7c96fe666b21ac245cabb
vignesh-yadav <vigneshyadav.dev@gmail.com>
2024-04-03 19:03:00 +0530

    updated header section

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import './Navbar.css'; 
 import { Link, useLocation } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { Avatar, Dropdown, } from 'flowbite-react';
 
 
 const Navbar = () => {
   const path = useLocation().pathname;
+  const { currentUser } = useSelector((state) => state.user);
+  const handleSignout = async () => {
+    try {
+      const res = await fetch('/api/user/signout', {
+        method: 'POST',
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        dispatch(signoutSuccess());
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <>
     <nav>
@@ -21,7 +39,33 @@ const Navbar = () => {
         <li><Link to="/about">About</Link></li>
         <li><Link to="/projects">Projects</Link></li>
       </ul>
+      
+      {currentUser ? (
+            <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar alt='user' img={currentUser.profilePicture} rounded />
+            }
+          >
+            <Dropdown.Header>
+              <span className='block text-sm'>@{currentUser.username}</span>
+              <span className='block text-sm font-medium truncate'>
+                {currentUser.email}
+              </span>
+            </Dropdown.Header>
+            <Link to={'/dashboard?tab=profile'}>
+              <Dropdown.Item>Profile</Dropdown.Item>
+            </Link>
+            <Dropdown.Divider />
+            <Dropdown.Item onClick={handleSignout}>Sign out</Dropdown.Item>
+          </Dropdown>
+       ) : 
+      (
       <button className="signin-button"><Link to="/sign-in">SignIn</Link></button> 
+        )
+      }
+
     </nav> 
     <div className="options">
        <ul className="menu2">
