8dfe62b5cc3ae86915a2445520e8b84a129f3343
vignesh-yadav <vigneshyadav.dev@gmail.com>
2024-04-07 18:06:53 +0530

    completed ui of profile page

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
-
+import { useSelector } from "react-redux";
+import { Alert, Button, Modal, ModalBody, TextInput } from 'flowbite-react';
+import "./Profile.css";
 export default function DashProfile() {
+  const { currentUser, error, loading } = useSelector((state) => state.user);
   return (
     <div className='other-allignment'>
-      profile
+    <div className="profile-tab">
+      <h1 className="title">Profile</h1>
+      <form className="profile-form">
+        <img src={currentUser.profilePicture} alt="avatar" className="userAvatar" />
+        <input className="profile-input" type="text" id="username" placeholder="username" defaultValue={currentUser.username}/>
+        <input className="profile-input" type="email" id="email" placeholder="email" defaultValue={currentUser.email}/>
+        <input className="profile-input" type="password" id="password" placeholder="password"/>
+        <button type="submit" id="profile-button" >Update</button>
+      </form>
+      <div className="del-out">
+        <span className='cursor-pointer delete-button'>
+          Delete Account
+        </span>
+        <span className='cursor-pointer out-button'>
+          Sign Out
+        </span>
+      </div>
+    </div>
     </div>
   )
 }
