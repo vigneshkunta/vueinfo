27cc7d7f853b84d70ac2c1b3e644038b9c7cd3f5
vignesh-yadav <vigneshyadav.dev@gmail.com>
2024-04-21 08:49:07 +0530

    added admin functionality

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -20,7 +20,12 @@ profilePicture: {
     default:
       'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
   },
-}, {timestamps: true}
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+},
+ {timestamps: true}
 );
 
 const User = mongoose.model('User', userSchema);
