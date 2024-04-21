bc79e2b1a7f7185012fdbae90041bcbed4d50e9a
vignesh-yadav <vigneshyadav.dev@gmail.com>
2024-04-21 15:53:42 +0530

    show users functionality

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import {
   deleteUser,
-//   getUser,
-//   getUsers,
+  getUser,
+  getUsers,
   signout,
   test,
   updateUser
@@ -15,7 +15,7 @@ router.get('/test', test);
 router.put('/update/:userId', verifyToken, updateUser);
 router.delete('/delete/:userId', verifyToken, deleteUser);
 router.post('/signout', signout);
-// router.get('/getusers', verifyToken, getUsers);
-// router.get('/:userId', getUser);
+router.get('/getusers', verifyToken, getUsers);
+router.get('/:userId', getUser);
 
 export default router;
